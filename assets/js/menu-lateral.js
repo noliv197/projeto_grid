1f2663cd6c2f3da1fc2c1a1f3e683db2a73fc399
Natalia Oliveira <63422199+noliv197@users.noreply.github.com>
2022-04-20 21:16:15 -0300

    consertei erro ddo menu lateral no .js

--- assets/js/menu-lateral.js
+++ assets/js/menu-lateral.js
@@ -2,5 +2,5 @@ var botao = document.querySelector(".cabecalho__menu");
 var menu = document.querySelector(".menu-lateral");
 
 botao.addEventListener("click",function(){
-    menu.classList.toggle("menu--ativo")
+    menu.classList.toggle("menu-lateral--ativo")
 })
