a6773c39569aa6d1e5c5e134621bcd41a0ca9169
Natalia Oliveira <63422199+noliv197@users.noreply.github.com>
2022-04-20 21:05:15 -0300

    upload de arquivos

---
+++ assets/js/menu-lateral.js
@@ -0,0 +1,6 @@
+var botao = document.querySelector(".cabecalho__menu");
+var menu = document.querySelector(".menu-lateral");
+
+botao.addEventListener("click",function(){
+    menu.classList.toggle("menu--ativo")
+})
